8d338f8189ccc4e6aa2d1b8cf7189d6903c4ccf0
mkervabo <mkervabo@student.42.fr>
2022-12-08 15:22:23 +0100

    add header and Home structure

---
+++ src/Header.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function Header() {
+  return (
+    <header className="Header">
+			<div className='Logo'>
+				Logo
+			</div>
+      <div className="Menu">
+				<div>Home</div>
+				<div>Palette Generator</div>
+				<div>My Palettes</div>
+			</div>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/Home.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function Home() {
+  return (
+    <div className="Home">
+      <h1>Create your own palette !</h1>
+			<div class='new-palette'>
+				<h2>Generate new palette</h2>
+				<div>
+					+
+				</div>
+			</div>
+			<div class='home-palette'>
+				<h2>My Palettes</h2>
+				<div></div>
+			</div>
+    </div>
+  )
+}
+
+export default Home
